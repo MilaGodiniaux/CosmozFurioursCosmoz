18e0a6c611f9a54636fa3b47e2323ecc66f4a4e6
Nicolas Pasquier <nicos@MacBook-Pro-de-Nicolas.local>
2018-11-26 18:00:30 +0100

    ajout des classes main paysage domino pioche suceptibles d'etres retirees plus tard

---
+++ src/Domino.java
@@ -0,0 +1,6 @@
+
+public class Domino {
+	int id;
+	int [] nbCouronnes = {0,0};
+	Paysage[] paysages;
+}
---
+++ src/Paysage.java
@@ -0,0 +1,12 @@
+
+public class Paysage {
+	int type;
+	/*
+	 * 1: champs
+	 * 2: foret
+	 * 3: mer
+	 * 4: prairie
+	 * 5: montagne
+	 * 6: mine
+	 */
+}
